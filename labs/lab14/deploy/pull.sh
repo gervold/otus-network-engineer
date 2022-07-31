4b6deb30f2c9ee7fec38bc470acc7bda9722eb08
gervold <gervold@mail.ru>
2022-08-01 00:41:48 +0300

    fix(lab14): fix LAB_FILE var in pull.sh

--- labs/lab14/deploy/pull.sh
+++ labs/lab14/deploy/pull.sh
@@ -3,7 +3,7 @@
 HOST=$1
 USER='root'  #auth by cert
 LAB_PATH='/opt/unetlab/tmp/0/b1c853e3-2c46-4f8a-8cdd-5f29eda621f6'
-LAB_FILE='/opt/unetlab/labs/lab13/ipsec.unl'
+LAB_FILE='/opt/unetlab/labs/lab14/ipsec.unl'
 
 #root@eve-ng:/opt/unetlab/tmp/0/d93fe442-2cfb-4fc7-bc19-0f374a2d3962# ls
 #1  10  11  12  13  14  15  16  17  18  19  2  20  21  22  23  24  25  26  27  28  29  3  30  31  32  4  5  7  8  9
